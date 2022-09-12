43d906c36d0fa38db00759b003a98cc3b0114a04
Angelo Badellino <badellinoa@gmail.com>
2022-09-12 16:04:42 +0200

    Homework01 complete

--- Homework/Homework01/Homework01.hs
+++ Homework/Homework01/Homework01.hs
@@ -2,15 +2,31 @@
 -- Question 1
 -- Write a multiline comment below.
 
+{-
+    This
+    is 
+    a
+    multiline
+    comment
+-}
+
 -- Question 2
 -- Define a function that takes a value and multiplies it by 3.
+
+multiplyBy3 x = x * 3
 
 -- Question 3
 -- Define a function that calculates the area of a circle.
 
+circleArea r = pi * r^2
+
 -- Question 4
 -- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder. 
 
+cilinderArea r h = circleArea r * h
+
 -- Question 5
 -- Define a function that checks if the volume of a cylinder is greater than or equal to 42.
+
+checkVolume r h = cilinderArea r h >=42
 
